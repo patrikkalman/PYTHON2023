95dd3e38cd83a362d3299f8491d0190076ccd32f
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2023-09-11 09:57:59 +0200

    Add files via upload

---
+++ sit0001.py
@@ -0,0 +1,24 @@
+print ("kalmanpatriklajosferencjozsipistatamasbencemukkerkukker")
+print ("    @")
+print ("   @@@")
+print ("  @@@@@")
+print (" @@@@@@@@")
+print ("@@@@@@@@@@")
+print ("====================")
+print ("||  kalman patrik ||")
+print ("||                ||")
+print ("||      10/b      ||")
+print ("||                ||")
+print ("||    2023.09.07  ||")
+print ("||                ||")
+print ("====================")
+print (" __
+        ||
+        || /|
+        ||/ |
+        |/  |
+        /   |
+       /    |
+       |    |
+       |    |
+       ------ ")
