f24634a0b34b3dfa3bd0a6e6e66ff4aa82f81d37
patrikkalman <120101231+patrikkalman@users.noreply.github.com>
2023-10-09 10:35:17 +0200

    Add files via upload

---
+++ LISTA10.09.py
@@ -0,0 +1,29 @@
+lista=["csoki","hacsok","negercsok","gumicukor"]
+
+#print(lista)
+
+for elem in lista:
+    print(elem)
+
+for elem in lista(:2):
+    print(elem)
+
+print("-"*30)
+
+for elem in lista(-2:):
+    print(elem)
+
+print("-"*30)
+
+for elem in lista(1::2):
+    print(elem)
+
+print("-"*30)
+
+for elem in lista(::-1):
+    print(elem)
+    
+print("-"*30)
+
+lista2=lista[:3]
+print(lista2)
